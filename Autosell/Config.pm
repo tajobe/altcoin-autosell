c14a52b9dd9b678f90ae936f078f680806c6e3b2
Taylor Becker <taylorjosephbecker@gmail.com>
2014-01-31 17:49:34 -0500

    Fixed tabbing/spacing and added project files to gitignore

--- Autosell/Config.pm
+++ Autosell/Config.pm
@@ -1,60 +1,60 @@
 #!/usr/bin/perl
-  
-  package Autosell::Config;
 
-  use warnings;
-  use strict;
-  use Exporter;
-  
-  use YAML::Tiny qw( LoadFile ); # parse yaml
-  
-  # exporting
-  our @ISA = qw( Exporter );
-  our @EXPORT_OK = qw( load ); # explicit export
-  
-  # logger
-  my $log = undef;
-  
-  ####################################################################################################
-  # Initialize config
-  # 
-  # Params:
-  #  file: Name+path of config file
-  #
-  # Returns self
-  ####################################################################################################
-  sub new
-  {
+package Autosell::Config;
+
+use warnings;
+use strict;
+use Exporter;
+
+use YAML::Tiny qw( LoadFile ); # parse yaml
+
+# exporting
+our @ISA = qw( Exporter );
+our @EXPORT_OK = qw( load ); # explicit export
+
+# logger
+my $log = undef;
+
+####################################################################################################
+# Initialize config
+# 
+# Params:
+#  file: Name+path of config file
+#
+# Returns self
+####################################################################################################
+sub new
+{
     my $class = shift;
     my $self =
     {
-      file       => shift || 'config.yml', # config filename;
-      log        => $log, # logger
-      poll       => 300, # seconds between polls(default 5 mins)
-      request    => 15, # request delay in seconds
-      target     => 'btc', # target/cashout currency
-      apikeys    => {}, # hash of exchange to API key/secret pair
-      coinmins   => {}, # min sell amounts for coins(optional, coin => amount)
-      excludes   => [] # array of coins to exclude from autosell
+        file       => shift || 'config.yml', # config filename;
+        log        => $log, # logger
+        poll       => 300, # seconds between polls(default 5 mins)
+        request    => 15, # request delay in seconds
+        target     => 'btc', # target/cashout currency
+        apikeys    => {}, # hash of exchange to API key/secret pair
+        coinmins   => {}, # min sell amounts for coins(optional, coin => amount)
+        excludes   => [] # array of coins to exclude from autosell
     };
-    
+  
     $log = Log::Log4perl->get_logger( __PACKAGE__ . '::' . $class );
     bless ( $self , $class );
     return $self;
-  }
-  
-  ####################################################################################################
-  # Load config file
-  ####################################################################################################
-  sub load
-  {
+}
+
+####################################################################################################
+# Load config file
+####################################################################################################
+sub load
+{
     my $self = shift || undef;
     
     # need self
     unless ( defined $self )
     {
-      $log->error( "Static reference not allowed!" );
-      return;
+        $log->error( "Static reference not allowed!" );
+        return;
     }
     
     # read config file
@@ -78,17 +78,17 @@
     my $pairfound = 0;
     foreach my $exchange ( keys % { $yaml->[0]->{ apikeys } } )
     {
-      $pairfound = 1; # found pair unless proven otherwise
-      
-      my $key = $yaml->[0]->{ apikeys }->{ $exchange }->{ key } || ($pairfound = 0);
-      my $secret = $yaml->[0]->{ apikeys }->{ $exchange }->{ secret } || ($pairfound = 0);
-      
-      if ( $pairfound )
-      {
-        $self->{ apikeys }->{ $exchange }->{ key } = $key;
-        $self->{ apikeys }->{ $exchange }->{ secret } = $secret;
-        $log->info( "API key pair found for exchange '$exchange'." );
-      }
+        $pairfound = 1; # found pair unless proven otherwise
+        
+        my $key = $yaml->[0]->{ apikeys }->{ $exchange }->{ key } || ($pairfound = 0);
+        my $secret = $yaml->[0]->{ apikeys }->{ $exchange }->{ secret } || ($pairfound = 0);
+        
+        if ( $pairfound )
+        {
+            $self->{ apikeys }->{ $exchange }->{ key } = $key;
+            $self->{ apikeys }->{ $exchange }->{ secret } = $secret;
+            $log->info( "API key pair found for exchange '$exchange'." );
+        }
     }
     
     # ensure we read at least one set of API keys
@@ -98,37 +98,37 @@
     $log->debug( "Checking for minimum sell amounts..." );
     foreach my $coin ( keys % { $yaml->[0]->{ coinmins } } )
     {
-      my $min = $yaml->[0]->{ coinmins }->{ $coin } || undef;
-      
-      if ( defined $min )
-      {
-        my $coinUC = uc $coin;
-        $log->info( "Setting minimum $coinUC sell amount to $min $coinUC." );
-        $self->{ coinmins }->{ $coin } = $min;
-      }
+        my $min = $yaml->[0]->{ coinmins }->{ $coin } || undef;
+        
+        if ( defined $min )
+        {
+            my $coinUC = uc $coin;
+            $log->info( "Setting minimum $coinUC sell amount to $min $coinUC." );
+            $self->{ coinmins }->{ $coin } = $min;
+        }
     }
     
     # excludes
     $log->debug( "Checking for excluded coins..." );
     for my $coin ( @ { $yaml->[0]->{ excludes } } )
     {
-      push( @ { $self->{ excludes } } , lc( $coin ) );
-      $log->info( "Excluding " . uc( $coin ) . " from auto-sell." );
+        push( @ { $self->{ excludes } } , lc( $coin ) );
+        $log->info( "Excluding " . uc( $coin ) . " from auto-sell." );
     }
-  }
-  
-  ####################################################################################################
-  # Load setting from the general section
-  # 
-  # Params:
-  #  yaml: YAML::Tiny object for reading config
-  #  config: key of setting to load from config
-  #  setting: Which setting to populate with what's loaded from config
-  #  matches: Optional matching string
-  #
-  ####################################################################################################
-  sub loadGeneralSetting
-  {
+}
+
+####################################################################################################
+# Load setting from the general section
+# 
+# Params:
+#  yaml: YAML::Tiny object for reading config
+#  config: key of setting to load from config
+#  setting: Which setting to populate with what's loaded from config
+#  matches: Optional matching string
+#
+####################################################################################################
+sub loadGeneralSetting
+{
     my $self = shift;
     my $yaml = shift;
     my $config = shift;
@@ -138,21 +138,20 @@
     my $value = lc( $yaml->[0]->{ general }->{ $config } ) || undef;
     if ( defined $value )
     {
-      if ( $value =~ /$matches/ )
-      {
-        $self->{ $setting } = $value;
-        $log->info( "Setting $config to " . uc( $value ) . "." );
-      }
-      else
-      {
-        $log->warn( "Unsupported ${config} value(${value})! Using default of " . uc( $self->{ $setting } ) . "." );
-      }
+        if ( $value =~ /$matches/ )
+        {
+            $self->{ $setting } = $value;
+            $log->info( "Setting $config to " . uc( $value ) . "." );
+        }
+        else
+        {
+            $log->warn( "Unsupported ${config} value(${value})! Using default of " . uc( $self->{ $setting } ) . "." );
+        }
     }
     else
     {
-      $log->warn( "Missing $config option! Using default of " . uc( $self->{ $setting } ) . "." );
+        $log->warn( "Missing $config option! Using default of " . uc( $self->{ $setting } ) . "." );
     }
+}
 
-  }
-  
-  1;
+1;
--- autoseller.pl
+++ autoseller.pl
@@ -1,49 +1,49 @@
 #!/usr/bin/perl
 
-  use warnings;
-  use strict;
+use warnings;
+use strict;
 
-  use JSON qw ( decode_json ); # JSON decode  
-  require Log::Log4perl; # logging
-  require LWP::UserAgent; # for requests
-  use Getopt::Long; # command-line options
-  
-  # our modules
-  use Autosell::Config qw( load ); # our config loader
-  
-  my $config = undef;
-  my $configFile = undef;  
+use JSON qw ( decode_json ); # JSON decode  
+require Log::Log4perl; # logging
+require LWP::UserAgent; # for requests
+use Getopt::Long; # command-line options
 
-  Log::Log4perl->init( "log.conf" );
-  my $log = Log::Log4perl->get_logger( "autoseller" );
-  $log->info;
-  $log->info( "Autoseller started." );
+# our modules
+use Autosell::Config qw( load ); # our config loader
 
-  GetOptions( "usage|help|h|u"               => \&usage ,
-              "config|file|configfile|cfg:s" => \$configFile );
-  
-  # init and load config
-  $config = Autosell::Config->new( $configFile );
-  $config->load();
+my $config = undef;
+my $configFile = undef;
 
-  ####################################################################################################
-  # usage
-  # 
-  # Print script usage
-  ####################################################################################################
-  sub usage
-  {
+Log::Log4perl->init( "log.conf" );
+my $log = Log::Log4perl->get_logger( "autoseller" );
+$log->info;
+$log->info( "Autoseller started." );
+
+GetOptions( "usage|help|h|u"               => \&usage ,
+            "config|file|configfile|cfg:s" => \$configFile );
+
+# init and load config
+$config = Autosell::Config->new( $configFile );
+$config->load();
+
+####################################################################################################
+# usage
+# 
+# Print script usage
+####################################################################################################
+sub usage
+{
     print "\n" .
-      "$0\n" .
-      "Autosell coins on an exchange\n" .
-      "\n" .
-      "Options:\n" .
-      "    -usage: Prints usage\n" .
-      "    -config=<filename>: Config file(optional)\n" .
-      "\n" .
-      "Usage:\n" .
-      "    ./$0\n" .
-      "    ./$0 -usage\n" .
-      "    ./$0 -config=<filename>\n" .
-      "\n";
-  }
+          "$0\n" .
+          "Autosell coins on an exchange\n" .
+          "\n" .
+          "Options:\n" .
+          "    -usage: Prints usage\n" .
+          "    -config=<filename>: Config file(optional)\n" .
+          "\n" .
+          "Usage:\n" .
+          "    ./$0\n" .
+          "    ./$0 -usage\n" .
+          "    ./$0 -config=<filename>\n" .
+          "\n";
+}
